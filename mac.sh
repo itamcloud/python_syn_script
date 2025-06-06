ce7ea381589b083c5eed3ff380f1c83777ea5424
sudheer01 <sudheerfriend01@gmail.com>
2025-06-06 16:51:06 +0530

    Add files via upload

---
+++ mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check Python version
+python3 --version
+
+# Create virtual environment
+python3 -m venv myenv
+
+# Activate virtual environment
+source myenv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run the main script
+python3 -m src.main
